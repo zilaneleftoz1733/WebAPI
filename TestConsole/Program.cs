21937daeaad02d978c4298573fc40cb282d196df
zilanelefoz <lzilaneleftozl@gmail.com>
2024-11-04 13:51:34 +0300

    Put ve delete islemi yapildi

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -9,10 +9,53 @@ namespace TestConsole
     {
         static async Task Main(string[] args)
         {
+
+            await NorthwindPost();
+             await NorthwindPut();
+            await NorthwindGet();
+            Console.WriteLine("***********Silme ****************");
+            //await NorthwindDelete();
+            //Console.WriteLine("*********** Get ****************");
+
             await NorthwindGet();
 
             Console.WriteLine("Hello, World!");
 
+
+        }
+        static async Task NorthwindDelete()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://northwind.now.sh");
+            var response = await client.DeleteAsync("/api/categories/7");
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("Silme islemi Basarili");
+            }
+            Console.WriteLine(response.StatusCode);
+        }
+        static async Task NorthwindPut()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://northwind.now.sh");
+            Category category = new Category()
+            {
+                Id = 61,
+                description = "Test Category",
+                name = "Test 1"
+            };
+            //var categoryJson = JsonSerializer.Serialize<Category>(category);
+
+
+            var response = await client.PutAsJsonAsync("/api/categories/1", category);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("Islem Basarili");
+            }
+            else
+            {
+                Console.WriteLine("Islem Başarisiz");
+            }
 
         }
         static async Task NorthwindPost()
@@ -21,17 +64,21 @@ namespace TestConsole
             client.BaseAddress = new Uri("https://northwind.now.sh");
             Category category = new Category()
             {
-                Id = 20,
+                Id = 61,
                 description = "Test Category",
                 name = "Test"
             };
-            var categoryJson = JsonSerializer.Serialize<Category>(category);
+            //var categoryJson = JsonSerializer.Serialize<Category>(category);
 
 
-            var response = await client.PostAsJsonAsync("/api/categories", categoryJson);
+            var response = await client.PostAsJsonAsync("/api/categories", category);
             if (response.StatusCode == HttpStatusCode.Created)
             {
                 Console.WriteLine("Islem Basarili");
+            }
+            else
+            {
+                Console.WriteLine("Islem Başarisiz");
             }
 
         }
